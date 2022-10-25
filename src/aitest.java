ffd964c759dd208fe5458b59eb946273100e68f2
kalyan <kalyankumar.k1808@gmail.com>
2022-10-25 07:15:57 +0530

    e

---
+++ src/aitest.java
@@ -0,0 +1,22 @@
+
+
+public class aitest {
+    static void printPattern(int n)
+    {
+        for (int i=0; i<n; i++)
+        {
+            for (int j=n; j<=0; j--)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[])
+    {
+        int n = 5;
+        printPattern(n);
+    }
+}
